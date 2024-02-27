f8f59355b08fa891b06a2331778d154a1cfb3e08
Victor Payno <vpayno@users.noreply.github.com>
2024-02-26 22:39:15 -0800

    learn_yaml: add yaml file output examples

--- src/learn_yaml/main.py
+++ src/learn_yaml/main.py
@@ -3,6 +3,7 @@
 from pprint import pprint
 from typing import Any
 
+import ruyaml
 import yaml
 from rich import pretty
 from rich import print as rprint
@@ -13,12 +14,71 @@ pretty.install()
 
 
 def load_yaml() -> dict[Any, Any]:
-    """Load TOML data from file"""
+    """Load Yaml data from a file"""
 
+    print("Reading config file from file.")
     with open("src/learn_yaml/config.yaml", "rb") as f:
         yaml_data: dict[Any, Any] = yaml.safe_load(f)
 
         return yaml_data
+
+
+def save_yaml(data: dict[Any, Any]) -> None:
+    """Save Yaml data to a file"""
+
+    print("Writing config file to file.")
+
+    if data is None:
+        data = {}
+
+    data["newkey"] = "new value"
+
+    print(data)
+    print()
+
+    with open("/tmp/pyyaml.yaml", "wb") as f:
+        yaml.dump(
+            data,
+            stream=f,
+            encoding="utf-8",
+            explicit_start=True,
+            explicit_end=None,
+            version=None,
+            tags=None,
+            indent=True,
+            width=4,
+            allow_unicode=True,
+            line_break=None,
+        )
+
+    with open("/tmp/ruyaml.yaml", "wb") as f:
+        ruyaml.dump(
+            data,
+            stream=f,
+            encoding="utf-8",
+            explicit_start=True,
+            explicit_end=None,
+            version=None,
+            tags=None,
+            indent=True,
+            width=4,
+            allow_unicode=True,
+            line_break=None,
+        )
+
+    with open("/tmp/pyyaml.yaml", "r") as f:
+        print("PyYaml Output file:")
+        for line in f.readlines():
+            print(line.rstrip())
+
+    print()
+
+    with open("/tmp/ruyaml.yaml", "r") as f:
+        print("RuYaml Output file:")
+        for line in f.readlines():
+            print(line.rstrip())
+
+    print()
 
 
 def main() -> None:
@@ -26,6 +86,9 @@ def main() -> None:
 
     # Load Toml data
     data: dict = load_yaml()
+
+    save_yaml(data)
+    print()
 
     print("Using built-in print()\n")
     print(data)
